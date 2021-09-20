91302dbf29860d5f4c51cb9e1776827fd3cc6161
Sergi Granell <xerpi.g.12@gmail.com>
2021-09-20 09:51:19 +0200

    New ReadyQ/RequestQ in place for USB Bulk In messages

--- arm/main.c
+++ arm/main.c
@@ -78,7 +78,7 @@ static u16 usb_intr_msg_handed_to_oh1_ioctlv_1_data ATTRIBUTE_ALIGN(32);
 static ioctlv usb_intr_msg_handed_to_oh1_ioctlvs[3] = {
 	{&usb_intr_msg_handed_to_oh1_ioctlv_0_data, sizeof(usb_intr_msg_handed_to_oh1_ioctlv_0_data)},
 	{&usb_intr_msg_handed_to_oh1_ioctlv_1_data, sizeof(usb_intr_msg_handed_to_oh1_ioctlv_1_data)},
-	{NULL, 0}, /* Filled dynamically */
+	{NULL, 0} /* Filled dynamically */
 };
 static ipcmessage usb_intr_msg_handed_to_oh1 ATTRIBUTE_ALIGN(32) = {
 	.command = IOS_IOCTLV,
@@ -90,38 +90,49 @@ static ipcmessage usb_intr_msg_handed_to_oh1 ATTRIBUTE_ALIGN(32) = {
 		.num_io = 1,
 		.vector = usb_intr_msg_handed_to_oh1_ioctlvs
 	}
-
 };
-static ipcmessage *ready_usb_intr_req_queue_data[8] ATTRIBUTE_ALIGN(32);
-static int ready_usb_intr_req_queue_id;
-static ipcmessage *pending_usb_intr_req_queue_data[8] ATTRIBUTE_ALIGN(32);
-static int pending_usb_intr_req_queue_id;
+
+static u8 usb_bulk_in_msg_handed_to_oh1_ioctlv_0_data ATTRIBUTE_ALIGN(32) = EP_ACL_DATA_IN;
+static u16 usb_bulk_in_msg_handed_to_oh1_ioctlv_1_data ATTRIBUTE_ALIGN(32);
+static ioctlv usb_bulk_in_msg_handed_to_oh1_ioctlvs[3] = {
+	{&usb_bulk_in_msg_handed_to_oh1_ioctlv_0_data, sizeof(usb_bulk_in_msg_handed_to_oh1_ioctlv_0_data)},
+	{&usb_bulk_in_msg_handed_to_oh1_ioctlv_1_data, sizeof(usb_bulk_in_msg_handed_to_oh1_ioctlv_1_data)},
+	{NULL, 0} /* Filled dynamically */
+};
+static ipcmessage usb_bulk_in_msg_handed_to_oh1 ATTRIBUTE_ALIGN(32) = {
+	.command = IOS_IOCTLV,
+	.result = IOS_OK,
+	.fd = 0, /* Filled dynamically */
+	.ioctlv = {
+		.command = USBV0_IOCTLV_BLKMSG,
+		.num_in = 2,
+		.num_io = 1,
+		.vector = usb_bulk_in_msg_handed_to_oh1_ioctlvs
+	}
+};
+
+static ipcmessage *ready_usb_intr_msg_queue_data[8] ATTRIBUTE_ALIGN(32);
+static int ready_usb_intr_msg_queue_id;
+static ipcmessage *pending_usb_intr_msg_queue_data[8] ATTRIBUTE_ALIGN(32);
+static int pending_usb_intr_msg_queue_id;
+
+static ipcmessage *ready_usb_bulk_in_msg_queue_data[8] ATTRIBUTE_ALIGN(32);
+static int ready_usb_bulk_in_msg_queue_id;
+static ipcmessage *pending_usb_bulk_in_msg_queue_data[8] ATTRIBUTE_ALIGN(32);
+static int pending_usb_bulk_in_msg_queue_id;
 
 static int ensure_init_oh1_context(void);
 
-/*static void print_ipcmessage(const ipcmessage *msg)
-{
-	svc_printf("command: 0x%x\n", msg->command);
-	svc_printf("result: 0x%x\n", msg->result);
-	svc_printf("fd: 0x%x\n", msg->fd);
-	svc_printf("ioctlv.command: 0x%x\n", msg->ioctlv.command);
-	svc_printf("ioctlv.num_in: 0x%x\n", msg->ioctlv.num_in);
-	svc_printf("ioctlv.num_io: 0x%x\n", msg->ioctlv.num_io);
-	svc_printf("ioctlv.vector: %p\n", msg->ioctlv.vector);
-	svc_printf("ioctlv.vecto[0].data: %p\n", msg->ioctlv.vector[0].data);
-	svc_printf("ioctlv.vecto[0].len: 0x%x\n", msg->ioctlv.vector[0].len);
-	svc_printf("ioctlv.vecto[1].data: %p\n", msg->ioctlv.vector[1].data);
-	svc_printf("ioctlv.vecto[1].len: 0x%x\n", msg->ioctlv.vector[1].len);
-	svc_printf("ioctlv.vecto[2].data: %p\n", msg->ioctlv.vector[2].data);
-	svc_printf("ioctlv.vecto[2].len: 0x%x\n", msg->ioctlv.vector[2].len);
-}*/
+/* PendingQ / ReadytQ helpers */
 
-static int configure_usb_intr_msg_handed_to_oh1(u32 fd, u16 min_wLength)
+static int configure_usb_bulk_intr_msg_handed_to_oh1(ipcmessage *handed_down_msg, u32 fd,
+						     u16 min_wLength)
 {
-	u16 *p_wLength = usb_intr_msg_handed_to_oh1.ioctlv.vector[1].data;
-	u32 *p_len = &usb_intr_msg_handed_to_oh1.ioctlv.vector[2].len;
-	void **p_data = &usb_intr_msg_handed_to_oh1.ioctlv.vector[2].data;
+	u16 *p_wLength = handed_down_msg->ioctlv.vector[1].data;
+	u32 *p_len = &handed_down_msg->ioctlv.vector[2].len;
+	void **p_data = &handed_down_msg->ioctlv.vector[2].data;
 
+	/* If the message has no data allocated, or it's smaller than the required... */
 	if (*p_data == NULL || *p_wLength < min_wLength) {
 		if (*p_data != NULL)
 			Mem_Free(*p_data);
@@ -133,56 +144,117 @@ static int configure_usb_intr_msg_handed_to_oh1(u32 fd, u16 min_wLength)
 		*p_len = *p_wLength;
 	}
 
-	usb_intr_msg_handed_to_oh1.fd = fd;
+	handed_down_msg->fd = fd;
 
 	os_sync_before_read(*p_data, *p_len);
 
 	return IOS_OK;
 }
 
-static inline u16 copy_ipcmessage_bulk_intr_ioctlv_data(ipcmessage *dst, const ipcmessage *src)
+static inline u16 copy_ipcmessage_bulk_intr_ioctlv_data(ipcmessage *dst, const ipcmessage *src,
+							bool is_intr)
 {
-	u16 src_wLength  = *(u16 *)src->ioctlv.vector[1].data;
-	u16 dst_wLength  = *(u16 *)dst->ioctlv.vector[1].data;
-	void *src_data   = src->ioctlv.vector[2].data;
-	void *dst_data   = dst->ioctlv.vector[2].data;
+	u32 copy_len;
+	void *src_data = src->ioctlv.vector[2].data;
+	void *dst_data = dst->ioctlv.vector[2].data;
 
-	assert(dst_wLength >= src_wLength);
+	if (is_intr) {
+		hci_event_hdr_t *event = src_data;
+		copy_len = sizeof(hci_event_hdr_t) + bswap16(event->length);
+	} else {
+		hci_acldata_hdr_t *hdr = src_data;
+		copy_len = sizeof(hci_acldata_hdr_t) + bswap16(hdr->length);
+	}
 
-	memcpy(dst_data, src_data, src_wLength);
-	os_sync_after_write(dst_data, src_wLength);
+	os_sync_before_read(src_data, copy_len);
+	memcpy(dst_data, src_data, copy_len);
+	os_sync_after_write(dst_data, copy_len);
 
-	return src_wLength;
+	return copy_len;
 }
 
-static int handle_oh1_dev_intr_message(ipcmessage *recv_msg, ipcmessage **ret_msg,
-				       u8 bEndpoint, u16 wLength, void *data)
+static int enqueue_recv_bulk_intr_pending_message(ipcmessage *recv_msg, ipcmessage **ret_msg,
+						  u16 recv_msg_len, int ready_queue_id,
+						  int pending_queue_id, ipcmessage *handed_down_msg,
+						  bool is_intr)
+{
+	int ret;
+	ipcmessage *ready_msg;
+
+	/* Fast-path: check if we already have a message ready to be delivered */
+	ret = os_message_queue_receive(ready_queue_id, &ready_msg, IOS_MESSAGE_NOBLOCK);
+	if (ret == IOS_OK) {
+		u16 ack_size = copy_ipcmessage_bulk_intr_ioctlv_data(recv_msg, ready_msg, is_intr);
+		ret = os_message_queue_ack(recv_msg, ack_size);
+	} else {
+		/* Hand down to OH1 a copy of the message to fill it from the real USB data */
+		ret = configure_usb_bulk_intr_msg_handed_to_oh1(handed_down_msg, recv_msg->fd,
+								recv_msg_len);
+		if (ret == IOS_OK) {
+			*ret_msg = handed_down_msg;
+			/* Push the received message to the PendingQ */
+			ret = os_message_queue_send(pending_queue_id,
+						    recv_msg, IOS_MESSAGE_NOBLOCK);
+		}
+	}
+
+	return ret;
+}
+
+/* Message handlers */
+
+static int handle_oh1_dev_bulk_message(ipcmessage *recv_msg, ipcmessage **ret_msg, u8 bEndpoint,
+				       u16 wLength, void *data)
+{
+	int ret = 0;
+	//svc_printf("BLK: bEndpoint 0x%x, wLength: 0x%x\n", bEndpoint, wLength);
+	//svc_printf("  data: 0x%x 0x%x\n", *(u32 *)data, *(u32 *)(data + 4));
+
+	if (bEndpoint == EP_ACL_DATA_OUT) {
+		/* HCI ACL data packet header */
+		/*hci_acldata_hdr_t *acl_hdr = data;
+		u16 acl_con_handle = HCI_CON_HANDLE(bswap16(acl_hdr->con_handle));
+		u16 acl_length     = bswap16(acl_hdr->length);
+		u8 *acl_payload    = data + sizeof(hci_acldata_hdr_t);*/
+
+		/* L2CAP header */
+		/*l2cap_hdr_t *l2cap_hdr = (void *)acl_payload;
+		u16 l2cap_length  = bswap16(l2cap_hdr->length);
+		u16 l2cap_dcid    = bswap16(l2cap_hdr->dcid);
+		u8 *l2cap_payload = acl_payload + sizeof(l2cap_hdr_t);
+
+		svc_printf("< ACL OUT: hdl: 0x%x, len: 0x%x\n", acl_con_handle, acl_length);*/
+
+		/* This is the ACL datapath from CPU to device (Wiimote) */
+		//ret = handle_acl_data_out_l2cap(acl_con_handle, l2cap_dcid,
+		//				 l2cap_payload, l2cap_length);
+	} else if (bEndpoint == EP_ACL_DATA_IN) {
+		//svc_printf("> ACL  IN: len: 0x%x\n", wLength);
+		/* We are given an ACL buffer to fill */
+		ret = enqueue_recv_bulk_intr_pending_message(recv_msg, ret_msg, wLength,
+							     ready_usb_bulk_in_msg_queue_id,
+							     pending_usb_bulk_in_msg_queue_id,
+							     &usb_bulk_in_msg_handed_to_oh1,
+							     false);
+	}
+
+	return ret;
+}
+
+static int handle_oh1_dev_intr_message(ipcmessage *recv_msg, ipcmessage **ret_msg, u8 bEndpoint,
+				       u16 wLength, void *data)
 {
 	int ret = 0;
 
-	svc_printf("INT: bEndpoint 0x%x, wLength: 0x%x\n", bEndpoint, wLength);
-	//svc_printf("  data: 0x%x 0x%x\n", *(u32 *)data, *(u32 *)(data + 4));
+	//svc_printf("INT: bEndpoint 0x%x, wLength: 0x%x\n", bEndpoint, wLength);
 
 	/* We are given a HCI buffer to fill */
 	if (bEndpoint == EP_HCI_EVENT) {
-		ipcmessage *ready_msg;
-		/* Fast-path: check if we already have a message ready to be delivered */
-		ret = os_message_queue_receive(ready_usb_intr_req_queue_id, &ready_msg,
-					       IOS_MESSAGE_NOBLOCK);
-		if (ret == IOS_OK) {
-			u16 size = copy_ipcmessage_bulk_intr_ioctlv_data(recv_msg, ready_msg);
-			ret = os_message_queue_ack(recv_msg, size);
-		} else {
-			/* Hand down to OH1 a "private" message  to fill it from the real USB */
-			ret = configure_usb_intr_msg_handed_to_oh1(recv_msg->fd, wLength);
-			if (ret == IOS_OK) {
-
-				*ret_msg = &usb_intr_msg_handed_to_oh1;
-				/* Push the received message to the PendingQ */
-				ret = os_message_queue_send(pending_usb_intr_req_queue_id,
-							    recv_msg, IOS_MESSAGE_NOBLOCK);
-			}
-		}
+		ret = enqueue_recv_bulk_intr_pending_message(recv_msg, ret_msg, wLength,
+							     ready_usb_intr_msg_queue_id,
+							     pending_usb_intr_msg_queue_id,
+							     &usb_intr_msg_handed_to_oh1,
+							     true);
 	}
 
 	return ret;
@@ -198,10 +270,27 @@ static int handle_oh1_dev_ioctlv(ipcmessage *recv_msg, ipcmessage **ret_msg,
 	InvalidateVector(vector, inlen, iolen);
 
 	switch (cmd) {
-	case USBV0_IOCTLV_CTRLMSG:
+#if 0
+	case USBV0_IOCTLV_CTRLMSG: {
+		u8 bmRequestType = *(u8 *)vector[0].data;
+		u8 bRequest      = *(u8 *)vector[1].data;
+		u16 wValue       = bswap16(*(u16 *)vector[2].data);
+		u16 wIndex       = bswap16(*(u16 *)vector[3].data);
+		u16 wLength      = bswap16(*(u16 *)vector[4].data);
+		// u16 wUnk      = *(u8 *)vector[5].data;
+		void *data       = vector[6].data;
+		ret = handle_oh1_dev_control_message(recv_msg, ret_msg, bmRequestType, bRequest,
+						     wValue, wIndex, wLength, data);
 		break;
-	case USBV0_IOCTLV_BLKMSG:
+	}
+#endif
+	case USBV0_IOCTLV_BLKMSG: {
+		u8 bEndpoint = *(u8 *)vector[0].data;
+		u16 wLength  = *(u16 *)vector[1].data;
+		void *data   = vector[2].data;
+		ret = handle_oh1_dev_bulk_message(recv_msg, ret_msg, bEndpoint, wLength, data);
 		break;
+	}
 	case USBV0_IOCTLV_INTRMSG: {
 		u8 bEndpoint = *(u8 *)vector[0].data;
 		u16 wLength  = *(u16 *)vector[1].data;
@@ -211,7 +300,7 @@ static int handle_oh1_dev_ioctlv(ipcmessage *recv_msg, ipcmessage **ret_msg,
 	}
 	default:
 		/* Unhandled/unknown ioctls are forwarded to the OH1 module */
-		svc_printf("Unhandled IOCTL: 0x%x\n", cmd);
+		//svc_printf("Unhandled IOCTL: 0x%x\n", cmd);
 		break;
 	}
 
@@ -267,32 +356,50 @@ static int OH1_IOS_ReceiveMessage_hook(int queueid, ipcmessage **ret_msg, u32 fl
 	return ret;
 }
 
+static int enqueue_recv_bulk_intr_ready_message(ipcmessage *ready_msg, int pending_queue_id,
+						int ready_queue_id, bool is_intr)
+{
+	int ret;
+	ipcmessage *pending_msg;
+
+	/* Fast-path: check if we have a PendingQ message to fill */
+	ret = os_message_queue_receive(pending_queue_id, &pending_msg, IOS_MESSAGE_NOBLOCK);
+	if (ret == IOS_OK) {
+		/* Copy message data */
+		u16 size = copy_ipcmessage_bulk_intr_ioctlv_data(pending_msg, ready_msg, is_intr);
+		ret = os_message_queue_ack(pending_msg, size);
+	} else {
+		/* Push message to ReadyQ */
+		ret = os_message_queue_send(ready_queue_id, ready_msg, IOS_MESSAGE_NOBLOCK);
+	}
+
+	return ret;
+}
+
 static int OH1_IOS_ResourceReply_hook(ipcmessage *ready_msg, int retval)
 {
 	int ret;
 
-	if (ready_msg == &usb_intr_msg_handed_to_oh1) {
-		ipcmessage *pending_msg;
-
-		ensure_init_oh1_context();
-
-		/* Fast-path: check if we have a PendingQ message to fill */
-		ret = os_message_queue_receive(pending_usb_intr_req_queue_id, &pending_msg,
-					       IOS_MESSAGE_NOBLOCK);
-		if (ret == IOS_OK) {
-			/* Copy message data */
-			u16 size = copy_ipcmessage_bulk_intr_ioctlv_data(pending_msg, ready_msg);
-			ret = os_message_queue_ack(pending_msg, size);
-		} else {
-			/* Push message to ReadyQ */
-			ret = os_message_queue_send(ready_usb_intr_req_queue_id,
-						    ready_msg, IOS_MESSAGE_NOBLOCK);
+	/* Only consider a message "ready" (valid) if it contains data (retval > 0) */
+	if (retval > 0) {
+		if (ready_msg == &usb_intr_msg_handed_to_oh1) {
+			ensure_init_oh1_context();
+			ret = enqueue_recv_bulk_intr_ready_message(ready_msg,
+								   pending_usb_intr_msg_queue_id,
+								   ready_usb_intr_msg_queue_id,
+								   true);
+			return ret;
+		} else if (ready_msg == &usb_bulk_in_msg_handed_to_oh1) {
+			ensure_init_oh1_context();
+			ret = enqueue_recv_bulk_intr_ready_message(ready_msg,
+								   pending_usb_bulk_in_msg_queue_id,
+								   ready_usb_bulk_in_msg_queue_id,
+								   false);
+			return ret;
 		}
-	} else {
-		ret = os_message_queue_ack(ready_msg, retval);
 	}
 
-	return ret;
+	return os_message_queue_ack(ready_msg, retval);
 }
 
 static int ensure_init_oh1_context(void)
@@ -302,17 +409,29 @@ static int ensure_init_oh1_context(void)
 
 	if (!initialized) {
 		/* Message queues can only be used on the process they were created in */
-		ret = os_message_queue_create(ready_usb_intr_req_queue_data,
-					      ARRAY_SIZE(ready_usb_intr_req_queue_data));
+		ret = os_message_queue_create(ready_usb_intr_msg_queue_data,
+					      ARRAY_SIZE(ready_usb_intr_msg_queue_data));
 		if (ret < 0)
 			return ret;
-		ready_usb_intr_req_queue_id = ret;
+		ready_usb_intr_msg_queue_id = ret;
 
-		ret = os_message_queue_create(pending_usb_intr_req_queue_data,
-					      ARRAY_SIZE(pending_usb_intr_req_queue_data));
+		ret = os_message_queue_create(pending_usb_intr_msg_queue_data,
+					      ARRAY_SIZE(pending_usb_intr_msg_queue_data));
 		if (ret < 0)
 			return ret;
-		pending_usb_intr_req_queue_id = ret;
+		pending_usb_intr_msg_queue_id = ret;
+
+		ret = os_message_queue_create(ready_usb_bulk_in_msg_queue_data,
+					      ARRAY_SIZE(ready_usb_bulk_in_msg_queue_data));
+		if (ret < 0)
+			return ret;
+		ready_usb_bulk_in_msg_queue_id = ret;
+
+		ret = os_message_queue_create(pending_usb_bulk_in_msg_queue_data,
+					      ARRAY_SIZE(pending_usb_bulk_in_msg_queue_data));
+		if (ret < 0)
+			return ret;
+		pending_usb_bulk_in_msg_queue_id = ret;
 
 		initialized = 1;
 	}
