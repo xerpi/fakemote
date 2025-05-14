617381df337c5d09c0779a86bd5a3fb4c8bb43f3
Alberto Mardegan <mardy@users.sourceforge.net>
2025-05-14 18:04:40 +0300

    embedded-game-controller: add interface for USB backends
    Convert the IOS code in usb_hid.c into a backend for the Wii.

--- embedded-game-controller/usb.h
+++ embedded-game-controller/usb.h
@@ -1,101 +1,103 @@
-#ifndef _USB2_H_
-#define _USB2_H_
+#ifndef EGC_USB_H
+#define EGC_USB_H
 
 #include "types.h"
 
 /* Constants */
-#define USB_MAXPATH 64
+#define EGC_USB_MAXPATH 64
 
-#define USB_OK     0
-#define USB_FAILED 1
+#define EGC_USB_OK     0
+#define EGC_USB_FAILED 1
 
-#define USB_CLASS_HID         0x03
-#define USB_SUBCLASS_BOOT     0x01
-#define USB_PROTOCOL_KEYBOARD 0x01
-#define USB_PROTOCOL_MOUSE    0x02
-#define USB_REPTYPE_INPUT     0x01
-#define USB_REPTYPE_OUTPUT    0x02
-#define USB_REPTYPE_FEATURE   0x03
-#define USB_REQTYPE_GET       0xA1
-#define USB_REQTYPE_SET       0x21
+#define EGC_USB_CLASS_HID         0x03
+#define EGC_USB_SUBCLASS_BOOT     0x01
+#define EGC_USB_PROTOCOL_KEYBOARD 0x01
+#define EGC_USB_PROTOCOL_MOUSE    0x02
+#define EGC_USB_REPTYPE_INPUT     0x01
+#define EGC_USB_REPTYPE_OUTPUT    0x02
+#define EGC_USB_REPTYPE_FEATURE   0x03
+#define EGC_USB_REQTYPE_GET       0xA1
+#define EGC_USB_REQTYPE_SET       0x21
 
 /* Descriptor types */
-#define USB_DT_DEVICE    0x01
-#define USB_DT_CONFIG    0x02
-#define USB_DT_STRING    0x03
-#define USB_DT_INTERFACE 0x04
-#define USB_DT_ENDPOINT  0x05
-#define USB_DT_HID       0x21
-#define USB_DT_REPORT    0x22
+#define EGC_USB_DT_DEVICE    0x01
+#define EGC_USB_DT_CONFIG    0x02
+#define EGC_USB_DT_STRING    0x03
+#define EGC_USB_DT_INTERFACE 0x04
+#define EGC_USB_DT_ENDPOINT  0x05
+#define EGC_USB_DT_HID       0x21
+#define EGC_USB_DT_REPORT    0x22
 
 /* Standard requests */
-#define USB_REQ_GETSTATUS     0x00
-#define USB_REQ_CLEARFEATURE  0x01
-#define USB_REQ_SETFEATURE    0x03
-#define USB_REQ_SETADDRESS    0x05
-#define USB_REQ_GETDESCRIPTOR 0x06
-#define USB_REQ_SETDESCRIPTOR 0x07
-#define USB_REQ_GETCONFIG     0x08
-#define USB_REQ_SETCONFIG     0x09
-#define USB_REQ_GETINTERFACE  0x0a
-#define USB_REQ_SETINTERFACE  0x0b
-#define USB_REQ_SYNCFRAME     0x0c
+#define EGC_USB_REQ_GETSTATUS     0x00
+#define EGC_USB_REQ_CLEARFEATURE  0x01
+#define EGC_USB_REQ_SETFEATURE    0x03
+#define EGC_USB_REQ_SETADDRESS    0x05
+#define EGC_USB_REQ_GETDESCRIPTOR 0x06
+#define EGC_USB_REQ_SETDESCRIPTOR 0x07
+#define EGC_USB_REQ_GETCONFIG     0x08
+#define EGC_USB_REQ_SETCONFIG     0x09
+#define EGC_USB_REQ_GETINTERFACE  0x0a
+#define EGC_USB_REQ_SETINTERFACE  0x0b
+#define EGC_USB_REQ_SYNCFRAME     0x0c
 
-#define USB_REQ_GETPROTOCOL 0x03
-#define USB_REQ_SETPROTOCOL 0x0B
-#define USB_REQ_GETREPORT   0x01
-#define USB_REQ_SETREPORT   0x09
+#define EGC_USB_REQ_GETPROTOCOL 0x03
+#define EGC_USB_REQ_SETPROTOCOL 0x0B
+#define EGC_USB_REQ_GETREPORT   0x01
+#define EGC_USB_REQ_SETREPORT   0x09
 
 /* Descriptor sizes per descriptor type */
-#define USB_DT_DEVICE_SIZE         18
-#define USB_DT_CONFIG_SIZE         9
-#define USB_DT_INTERFACE_SIZE      9
-#define USB_DT_ENDPOINT_SIZE       7
-#define USB_DT_ENDPOINT_AUDIO_SIZE 9 /* Audio extension */
-#define USB_DT_HID_SIZE            9
-#define USB_DT_HUB_NONVAR_SIZE     7
+#define EGC_USB_DT_DEVICE_SIZE         18
+#define EGC_USB_DT_CONFIG_SIZE         9
+#define EGC_USB_DT_INTERFACE_SIZE      9
+#define EGC_USB_DT_ENDPOINT_SIZE       7
+#define EGC_USB_DT_ENDPOINT_AUDIO_SIZE 9 /* Audio extension */
+#define EGC_USB_DT_HID_SIZE            9
+#define EGC_USB_DT_HUB_NONVAR_SIZE     7
 
 /* control message request type bitmask */
-#define USB_CTRLTYPE_DIR_HOST2DEVICE (0 << 7)
-#define USB_CTRLTYPE_DIR_DEVICE2HOST (1 << 7)
-#define USB_CTRLTYPE_TYPE_STANDARD   (0 << 5)
-#define USB_CTRLTYPE_TYPE_CLASS      (1 << 5)
-#define USB_CTRLTYPE_TYPE_VENDOR     (2 << 5)
-#define USB_CTRLTYPE_TYPE_RESERVED   (3 << 5)
-#define USB_CTRLTYPE_REC_DEVICE      0
-#define USB_CTRLTYPE_REC_INTERFACE   1
-#define USB_CTRLTYPE_REC_ENDPOINT    2
-#define USB_CTRLTYPE_REC_OTHER       3
+#define EGC_USB_CTRLTYPE_DIR_HOST2DEVICE (0 << 7)
+#define EGC_USB_CTRLTYPE_DIR_DEVICE2HOST (1 << 7)
+#define EGC_USB_CTRLTYPE_TYPE_STANDARD   (0 << 5)
+#define EGC_USB_CTRLTYPE_TYPE_CLASS      (1 << 5)
+#define EGC_USB_CTRLTYPE_TYPE_VENDOR     (2 << 5)
+#define EGC_USB_CTRLTYPE_TYPE_RESERVED   (3 << 5)
+#define EGC_USB_CTRLTYPE_REC_DEVICE      0
+#define EGC_USB_CTRLTYPE_REC_INTERFACE   1
+#define EGC_USB_CTRLTYPE_REC_ENDPOINT    2
+#define EGC_USB_CTRLTYPE_REC_OTHER       3
 
-#define USB_REQTYPE_INTERFACE_GET                                                                  \
-    (USB_CTRLTYPE_DIR_DEVICE2HOST | USB_CTRLTYPE_TYPE_CLASS | USB_CTRLTYPE_REC_INTERFACE)
-#define USB_REQTYPE_INTERFACE_SET                                                                  \
-    (USB_CTRLTYPE_DIR_HOST2DEVICE | USB_CTRLTYPE_TYPE_CLASS | USB_CTRLTYPE_REC_INTERFACE)
-#define USB_REQTYPE_ENDPOINT_GET                                                                   \
-    (USB_CTRLTYPE_DIR_DEVICE2HOST | USB_CTRLTYPE_TYPE_CLASS | USB_CTRLTYPE_REC_ENDPOINT)
-#define USB_REQTYPE_ENDPOINT_SET                                                                   \
-    (USB_CTRLTYPE_DIR_HOST2DEVICE | USB_CTRLTYPE_TYPE_CLASS | USB_CTRLTYPE_REC_ENDPOINT)
+#define EGC_USB_REQTYPE_INTERFACE_GET                                                              \
+    (EGC_USB_CTRLTYPE_DIR_DEVICE2HOST | EGC_USB_CTRLTYPE_TYPE_CLASS |                              \
+     EGC_USB_CTRLTYPE_REC_INTERFACE)
+#define EGC_USB_REQTYPE_INTERFACE_SET                                                              \
+    (EGC_USB_CTRLTYPE_DIR_HOST2DEVICE | EGC_USB_CTRLTYPE_TYPE_CLASS |                              \
+     EGC_USB_CTRLTYPE_REC_INTERFACE)
+#define EGC_USB_REQTYPE_ENDPOINT_GET                                                               \
+    (EGC_USB_CTRLTYPE_DIR_DEVICE2HOST | EGC_USB_CTRLTYPE_TYPE_CLASS | EGC_USB_CTRLTYPE_REC_ENDPOINT)
+#define EGC_USB_REQTYPE_ENDPOINT_SET                                                               \
+    (EGC_USB_CTRLTYPE_DIR_HOST2DEVICE | EGC_USB_CTRLTYPE_TYPE_CLASS | EGC_USB_CTRLTYPE_REC_ENDPOINT)
 
-#define USB_FEATURE_ENDPOINT_HALT 0
+#define EGC_USB_FEATURE_ENDPOINT_HALT 0
 
-#define USB_ENDPOINT_INTERRUPT 0x03
-#define USB_ENDPOINT_IN        0x80
-#define USB_ENDPOINT_OUT       0x00
+#define EGC_USB_ENDPOINT_INTERRUPT 0x03
+#define EGC_USB_ENDPOINT_IN        0x80
+#define EGC_USB_ENDPOINT_OUT       0x00
 
-#define USB_OH0_DEVICE_ID 0x00000000 // for completion
-#define USB_OH1_DEVICE_ID 0x00200000
+#define EGC_USB_OH0_DEVICE_ID 0x00000000 // for completion
+#define EGC_USB_OH1_DEVICE_ID 0x00200000
 
 /* Structures */
-typedef struct _usbendpointdesc {
+typedef struct egc_usb_endpointdesc_t {
     u8 bLength;
     u8 bDescriptorType;
     u8 bEndpointAddress;
     u8 bmAttributes;
     u16 wMaxPacketSize;
     u8 bInterval;
-} ATTRIBUTE_PACKED usb_endpointdesc;
+} ATTRIBUTE_PACKED egc_usb_endpointdesc_t;
 
-typedef struct _usbinterfacedesc {
+typedef struct egc_usb_interfacedesc_t {
     u8 bLength;
     u8 bDescriptorType;
     u8 bInterfaceNumber;
@@ -107,10 +109,10 @@ typedef struct _usbinterfacedesc {
     u8 iInterface;
     u8 *extra;
     u8 extra_size;
-    struct _usbendpointdesc *endpoints;
-} ATTRIBUTE_PACKED usb_interfacedesc;
+    struct egc_usb_endpointdesc_t *endpoints;
+} ATTRIBUTE_PACKED egc_usb_interfacedesc_t;
 
-typedef struct _usbconfdesc {
+typedef struct egc_usb_confdesc_t {
     u8 bLength;
     u8 bDescriptorType;
     u16 wTotalLength;
@@ -119,10 +121,10 @@ typedef struct _usbconfdesc {
     u8 iConfiguration;
     u8 bmAttributes;
     u8 bMaxPower;
-    struct _usbinterfacedesc *interfaces;
-} ATTRIBUTE_PACKED usb_configurationdesc;
+    struct egc_usb_interfacedesc_t *interfaces;
+} ATTRIBUTE_PACKED egc_usb_configurationdesc_t;
 
-typedef struct _usbdevdesc {
+typedef struct egc_usb_devdesc_t {
     u8 bLength;
     u8 bDescriptorType;
     u16 bcdUSB;
@@ -137,10 +139,9 @@ typedef struct _usbdevdesc {
     u8 iProduct;
     u8 iSerialNumber;
     u8 bNumConfigurations;
-    struct _usbconfdesc *configurations;
-} ATTRIBUTE_PACKED usb_devdesc;
+} ATTRIBUTE_PACKED egc_usb_devdesc_t;
 
-typedef struct _usbhiddesc {
+typedef struct egc_usb_hiddesc_t {
     u8 bLength;
     u8 bDescriptorType;
     u16 bcdHID;
@@ -150,13 +151,35 @@ typedef struct _usbhiddesc {
         u8 bDescriptorType;
         u16 wDescriptorLength;
     } descr[1];
-} ATTRIBUTE_PACKED usb_hiddesc;
+} ATTRIBUTE_PACKED egc_usb_hiddesc_t;
 
-typedef struct _usb_device_entry {
-    s32 device_id;
-    u16 vid;
-    u16 pid;
-    u32 token;
-} usb_device_entry;
+typedef struct egc_usb_device_t egc_usb_device_t;
+
+typedef enum egc_usb_transfer_type_e {
+    EGC_USB_TRANSFER_CONTROL,
+    EGC_USB_TRANSFER_ISOCHRONOUS,
+    EGC_USB_TRANSFER_BULK,
+    EGC_USB_TRANSFER_INTERRUPT,
+} egc_usb_transfer_type_e;
+
+typedef enum egc_usb_transfer_status_e {
+    EGC_USB_TRANSFER_STATUS_UNSET = 0,
+    EGC_USB_TRANSFER_STATUS_COMPLETED,
+    EGC_USB_TRANSFER_STATUS_ERROR,
+} egc_usb_transfer_status_e;
+
+typedef struct egc_usb_transfer_t egc_usb_transfer_t;
+
+struct egc_usb_transfer_t {
+    egc_usb_device_t *device;
+    egc_usb_transfer_type_e transfer_type;
+    egc_usb_transfer_status_e status;
+    u8 endpoint;
+    u16 length;  /* length of actually used data */
+    u16 bufsize; /* data buffer size */
+    u8 *data;
+} ATTRIBUTE_PACKED;
+
+typedef void (*egc_transfer_cb)(egc_usb_transfer_t *transfer);
 
 #endif
